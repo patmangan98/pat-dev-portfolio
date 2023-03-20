262995a7795326dfa40e49e8d1b4b02384b931a6
Patrick Mangan <patrickmangan@Patricks-Mini.lan>
2023-03-20 15:19:39 -0400

    almost ready for deployment

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,15 @@ import {
 } from "react-icons/ai";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
+import { pdfjs } from "react-pdf";
 import deved from "../public/peepsAvatar.png";
 import Image from "next/image";
 import BarkBoard from '../public/images/BarkBoard.png'
 import Jobbr3d from '../public/images/Jobbr3d.png'
 import Ribbit from '../public/images/Ribbit.png'
 import SpaceMan from '../public/images/SpaceMan.png'
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -41,14 +44,7 @@ export default function Home() {
                   className=" cursor-pointer text-2xl"
                 />
               </li>
-              <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
-                >
-                  Resume
-                </a>
-              </li>
+      
             </ul>
           </nav>
           <div className="text-center p-10 py-10">
@@ -72,15 +68,15 @@ export default function Home() {
         </section>
         <section className="py-10">
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since transitioning careers, I have built multiple projects as you can see below. Hover over the project to read more, and click to Check it out!
+            <h3 className="text-3xl text-center py-1 dark:text-white ">Portofolio</h3>
+            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+              Since transitioning careers, I have built multiple projects as you can see below. Hover/Tap on the project to find out more!
             </p>
           </div>
 
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 " onMouseEnter={() => setShowParagraph1(false)}
-              onMouseLeave={() => setShowParagraph1(true)}>
+            <div className="basis-1/3 flex-1 " onMouseOver={() => setShowParagraph1(false)}
+              onMouseOut={() => setShowParagraph1(true)}>
               {showParagraph1 ? (
                 <Image
                   className="rounded-lg object-cover"
@@ -176,6 +172,12 @@ export default function Home() {
             </div>
           </div>
         </section>
+            <section>
+              <div className="py-10 text-center">
+                <h3 className ="text-xl py-12 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">Resume</h3>
+                <iframe  width ="100%" height="500px"src="/images/NewResume2023.pdf"/>
+              </div>
+            </section>
       </main>
     </div>
   );
